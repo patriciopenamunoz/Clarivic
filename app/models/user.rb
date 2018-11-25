374abac80b3ce597dc7575140a369566e4e5805c
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-11-25 16:00:50 -0300

    Add enums fields to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum gender: %i[masculine femenine]
+  enum access_level: %i[user admin]
 end

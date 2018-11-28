c9c79286e83fc46f11cfa61eb19abb7a24e9f2c4
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-11-28 20:53:44 -0300

    Add messages for db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,20 @@ require 'open-uri'
 Hostel.destroy_all
 User.destroy_all
 
-user = User.new
-user.email = 'admin@admin.com'
-user.password = '123456'
-user.password_confirmation = '123456'
-user.first_name = Faker::Name.first_name
-user.middle_name = Faker::Name.middle_name
-user.first_surname = Faker::Name.last_name
-user.second_surname = Faker::Name.last_name
-user.gender = User.genders.keys.sample
-user.bio = Faker::Hipster.paragraph
-user.access_level = 'admin'
+admin = User.new
+admin.email = 'admin@admin.com'
+admin.password = '123456'
+admin.password_confirmation = '123456'
+admin.first_name = Faker::Name.first_name
+admin.middle_name = Faker::Name.middle_name
+admin.first_surname = Faker::Name.last_name
+admin.second_surname = Faker::Name.last_name
+admin.gender = User.genders.keys.sample
+admin.bio = Faker::Hipster.paragraph
+admin.access_level = 'admin'
+admin.save
 
-10.times do
+10.times do |i|
   user = User.new
   user.email = Faker::Internet.email
   user.password = '123456'
@@ -33,9 +34,13 @@ user.access_level = 'admin'
   user.second_surname = Faker::Name.last_name
   user.gender = User.genders.keys.sample
   user.bio = Faker::Hipster.paragraph
+  puts "[#{(i + 1)}/10] User generated: #{user.email}"
   # Define if the created user has hostels by a random boolean value.
   while [true, false].sample
-    file = open("https://loremflickr.com/1280/960/hostel")
+    puts '- Adding a Hostel for this user...'
+    puts '- Downloading image from https://loremflickr.com/1280/960/hostel'
+    file = open('https://loremflickr.com/1280/960/hostel')
+    puts '- DONE! Image downloaded'
     hostel = Hostel.new
     hostel.user = user
     hostel.name = Faker::FunnyName.name
@@ -47,5 +52,11 @@ user.access_level = 'admin'
       filename: "hostel_principal_image_#{hostel.id}.jpg",
       content_type: 'image/jpg'
     )
+    puts "- Hostel generated for #{user.email}: #{hostel.name}"
   end
 end
+puts '###############################'
+puts 'Seed generated:'
+puts "Admin email: #{admin.email}"
+puts "Admin password: #{admin.password}"
+puts ''

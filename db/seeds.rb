5fb49b50bf011c5fa7a6a2b2ea97ad810dd2a537
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-11-28 17:37:15 -0300

    ADD seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Hostel.destroy_all
+User.destroy_all
+
+user = User.new
+user.email = 'admin@admin.com'
+user.password = '123456'
+user.password_confirmation = '123456'
+user.first_name = Faker::Name.first_name
+user.middle_name = Faker::Name.middle_name
+user.first_surname = Faker::Name.last_name
+user.second_surname = Faker::Name.last_name
+user.gender = User.genders.keys.sample
+user.bio = Faker::Hipster.paragraph
+user.access_level = 'admin'
+
+10.times do
+  user = User.new
+  user.email = Faker::Internet.email
+  user.password = '123456'
+  user.password_confirmation = '123456'
+  user.first_name = Faker::Name.first_name
+  user.middle_name = Faker::Name.middle_name
+  user.first_surname = Faker::Name.last_name
+  user.second_surname = Faker::Name.last_name
+  user.gender = User.genders.keys.sample
+  user.bio = Faker::Hipster.paragraph
+  # Define if the created user has hostels by a random boolean value.
+  while [true, false].sample
+    file = open("https://loremflickr.com/1280/960/hostel")
+    hostel = Hostel.new
+    hostel.user = user
+    hostel.name = Faker::FunnyName.name
+    hostel.address = Faker::Address.street_address
+    hostel.description = Faker::Hipster.paragraph
+    hostel.save
+    hostel.principal_image.attach(
+      io: file,
+      filename: "hostel_principal_image_#{hostel.id}.jpg",
+      content_type: 'image/jpg'
+    )
+  end
+end

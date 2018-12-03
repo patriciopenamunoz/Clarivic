3f38f9a07f9eeb385c3587bcb54ccf67e5fc7641
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-12-03 20:20:21 -0300

    Set env conditions for seed generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,53 +10,55 @@ require 'open-uri'
 Hostel.destroy_all
 User.destroy_all
 
-admin = User.new
-admin.email = 'admin@admin.com'
-admin.password = '123456'
-admin.password_confirmation = '123456'
-admin.first_name = Faker::Name.first_name
-admin.middle_name = Faker::Name.middle_name
-admin.first_surname = Faker::Name.last_name
-admin.second_surname = Faker::Name.last_name
-admin.gender = User.genders.keys.sample
-admin.bio = Faker::Hipster.paragraph
-admin.access_level = 'admin'
-admin.save
+if Rails.env
+  admin = User.new
+  admin.email = 'admin@admin.com'
+  admin.password = '123456'
+  admin.password_confirmation = '123456'
+  admin.first_name = Faker::Name.first_name
+  admin.middle_name = Faker::Name.middle_name
+  admin.first_surname = Faker::Name.last_name
+  admin.second_surname = Faker::Name.last_name
+  admin.gender = User.genders.keys.sample
+  admin.bio = Faker::Hipster.paragraph
+  admin.access_level = 'admin'
+  admin.save
 
-10.times do |i|
-  user = User.new
-  user.email = Faker::Internet.email
-  user.password = '123456'
-  user.password_confirmation = '123456'
-  user.first_name = Faker::Name.first_name
-  user.middle_name = Faker::Name.middle_name
-  user.first_surname = Faker::Name.last_name
-  user.second_surname = Faker::Name.last_name
-  user.gender = User.genders.keys.sample
-  user.bio = Faker::Hipster.paragraph
-  puts "[#{(i + 1)}/10] User generated: #{user.email}"
-  # Define if the created user has hostels by a random boolean value.
-  while [true, false].sample
-    puts '- Adding a Hostel for this user...'
-    puts '- Downloading image from https://loremflickr.com/1280/960/hostel'
-    file = open('https://loremflickr.com/1280/960/hostel')
-    puts '- DONE! Image downloaded'
-    hostel = Hostel.new
-    hostel.user = user
-    hostel.name = Faker::FunnyName.name
-    hostel.address = Faker::Address.street_address
-    hostel.description = Faker::Hipster.paragraph
-    hostel.save
-    hostel.principal_image.attach(
-      io: file,
-      filename: "hostel_principal_image_#{hostel.id}.jpg",
-      content_type: 'image/jpg'
-    )
-    puts "- Hostel generated for #{user.email}: #{hostel.name}"
+  10.times do |i|
+    user = User.new
+    user.email = Faker::Internet.email
+    user.password = '123456'
+    user.password_confirmation = '123456'
+    user.first_name = Faker::Name.first_name
+    user.middle_name = Faker::Name.middle_name
+    user.first_surname = Faker::Name.last_name
+    user.second_surname = Faker::Name.last_name
+    user.gender = User.genders.keys.sample
+    user.bio = Faker::Hipster.paragraph
+    puts "[#{(i + 1)}/10] User generated: #{user.email}"
+    # Define if the created user has hostels by a random boolean value.
+    while [true, false].sample
+      puts '- Adding a Hostel for this user...'
+      puts '- Downloading image from https://loremflickr.com/1280/960/hostel'
+      file = open('https://loremflickr.com/1280/960/hostel')
+      puts '- DONE! Image downloaded'
+      hostel = Hostel.new
+      hostel.user = user
+      hostel.name = Faker::FunnyName.name
+      hostel.address = Faker::Address.street_address
+      hostel.description = Faker::Hipster.paragraph
+      hostel.save
+      hostel.principal_image.attach(
+        io: file,
+        filename: "hostel_principal_image_#{hostel.id}.jpg",
+        content_type: 'image/jpg'
+      )
+      puts "- Hostel generated for #{user.email}: #{hostel.name}"
+    end
   end
+  puts '###############################'
+  puts 'Seed generated:'
+  puts "Admin email: #{admin.email}"
+  puts "Admin password: #{admin.password}"
+  puts ''
 end
-puts '###############################'
-puts 'Seed generated:'
-puts "Admin email: #{admin.email}"
-puts "Admin password: #{admin.password}"
-puts ''

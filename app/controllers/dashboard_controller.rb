a2e76394f97e84c5ad9f955432a83047115ff8a2
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-12-04 23:58:43 -0300

    List user's hostels

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @my_hostels = current_user.hostel_registrations.where(role: 'admin')
+    @my_hostels = @my_hostels.map { |h| h.hostel }
   end
 end

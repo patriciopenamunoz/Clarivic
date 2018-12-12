b20c98f733ff124bf055c1d22cf2b1aeeedd45b2
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-12-12 20:46:32 -0300

    Add parammeters into changeCommunes()

--- app/assets/javascripts/hostels.js
+++ app/assets/javascripts/hostels.js
@@ -1,12 +1,14 @@
-function changeCommunes() {
-  $('#hostel_commune_id>option').remove();
+function changeCommunes(regions_select, communes_select) {
+  let communes_fist_value = $('option', communes_select)[0];
+  $('option', communes_select).remove();
+  $(communes_select).append(communes_fist_value);
   $.get({
-    url: '/api/region/'+$('#hostel_region_id').val(),
+    url: '/api/region/'+regions_select.value,
     dataType: 'json'
   })
   .done(function(json) {
     json.communes.forEach((e) => {
-      $('#hostel_commune_id').append(new Option(e.short_name, e.id));
+      $(communes_select).append(new Option(e.short_name, e.id));
     });
   })
   .fail(function(error) { console.log(error) });

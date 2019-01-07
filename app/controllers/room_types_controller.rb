973d8871fcd2613a3d5f21a85305291542027727
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-07 20:15:03 -0300

    Add strong params to room_types

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -1,5 +1,13 @@
 class RoomTypesController < ApplicationController
   def index
-    
+
+  end
+
+  private
+
+  def room_type_params
+    params.require(:room_type).permit(%i[name description number_rooms
+                                         occupied_rooms value_by_night
+                                         hostel_id])
   end
 end

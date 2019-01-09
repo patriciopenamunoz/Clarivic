d500ac886addd771554a0e448feff8cc96cfe075
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-09 20:21:36 -0300

    Allow to add new room types

--- app/controllers/room_types_controller.rb
+++ app/controllers/room_types_controller.rb
@@ -1,6 +1,22 @@
 class RoomTypesController < ApplicationController
   def index
+    @room_types = RoomType.where(hostel_id: params[:hostel_id])
+  end
 
+  def new
+    @hostel = Hostel.find(params[:hostel_id])
+    @room_type = RoomType.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    params[:room_type][:hostel_id] = params[:hostel_id]
+    @room_type = RoomType.create(room_type_params)
+    respond_to do |format|
+      format.js
+    end
   end
 
   private

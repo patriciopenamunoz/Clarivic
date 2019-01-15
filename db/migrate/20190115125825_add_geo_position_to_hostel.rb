2fd3da1f58d7beeb4ed6e21d0bffe22d4eaba004
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-15 10:45:53 -0300

    Add latitude and longitude attributes to Hostel model.

---
+++ db/migrate/20190115125825_add_geo_position_to_hostel.rb
@@ -0,0 +1,6 @@
+class AddGeoPositionToHostel < ActiveRecord::Migration[5.2]
+  def change
+    add_column :hostels, :latitude, :float
+    add_column :hostels, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_225436) do
+ActiveRecord::Schema.define(version: 2019_01_15_125825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2019_01_12_225436) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "commune_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["commune_id"], name: "index_hostels_on_commune_id"
   end
 

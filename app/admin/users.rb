7ff484978cd5bf335008517ffc009d6220b8623d
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-15 23:34:04 -0300

    Configure User Resource

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -12,4 +12,47 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+  permit_params :first_name, :middle_name, :first_surname, :second_surname,
+                :gender, :bio, :access_level, :email, :password
+
+  index do
+    column :id
+    column :email
+    column :created_at
+    column :hostel_count { |user| user.hostels.length }
+
+    actions
+  end
+
+  form do |f|
+    inputs 'Agregando nuevo usuario' do
+      input :first_name
+      input :middle_name
+      input :first_surname
+      input :second_surname
+      input :gender
+      input :bio
+      input :access_level
+      input :email
+      input :password
+    end
+
+    actions
+  end
+
+controller do
+  def update
+    user = params[:user]
+    if (user[:password].blank? && user[:password_confirmation].blank?)
+      params[:user].delete('password')
+      params[:user].delete('password_confirmation')
+    end
+    super
+  end
+end
+
+filter :id
+filter :email
+filter :created_at
+
 end

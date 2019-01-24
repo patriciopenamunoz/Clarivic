7b787cd4de65a46aa739cd555ee8980dae55780f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-23 22:00:24 -0300

    Add methods to evaluate Reservation dates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ApplicationRecord
   belongs_to :hostel_registration
   has_many :reserved_rooms
+
+  def started?(date: Date.current)
+    date >= starting_date
+  end
+
+  def expired?(date: Date.current)
+    date > ending_date
+  end
+
+  def in_date?(date: Date.current)
+    started?(date: date) && !expired?(date: date)
+  end
 end

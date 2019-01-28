c49e4f927c635dad39112f9ffbb1dba4c1bc3cb8
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-28 13:24:02 -0300

    Add rooms_in_range method for room_type model

--- app/models/room_type.rb
+++ app/models/room_type.rb
@@ -7,4 +7,14 @@ class RoomType < ApplicationRecord
   def occupied_rooms(date: Date.current)
     reserved_rooms.map { |r| r.reservation.in_date?(date: date) ? r.quantity : 0 }.sum
   end
+
+  def rooms_in_range(starting_date: Date.current, ending_date: Date.current + 1)
+    date_counter = starting_date
+    rooms_stack = []
+    while date_counter <= ending_date
+      rooms_stack += [occupied_rooms(date: date_counter)]
+      date_counter += 1
+    end
+    rooms_stack.max
+  end
 end

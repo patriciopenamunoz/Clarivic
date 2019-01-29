9ea96c4bac19a609fcf092c502e438b5e04d1b6c
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-01-28 21:30:40 -0300

    Allow to set date ranges into hostel#index

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,21 @@
 class ReservationsController < ApplicationController
+  before_action :set_date, only: %i[set_dates set_dates_and_redirect]
+
+  def set_dates
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def set_dates_and_redirect
-    session[:starting_date] = params[:starting_date]
-    session[:ending_date] = params[:ending_date]
     redirect_to controller: 'hostels', action: 'index',
                             region_id: params[:region_id]
+  end
+
+  private
+
+  def set_date
+    session[:starting_date] = params[:starting_date]
+    session[:ending_date] = params[:ending_date]
   end
 end

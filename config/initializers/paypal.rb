c122b38a7b8353952ee3850825dce65e5dd6e5ba
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-02-04 19:41:03 -0300

    Use credentials into paypal.rb

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,5 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
+PayPal::SDK.configure(
+  client_id: Rails.application.credentials.paypal_client_id,
+  client_secret: Rails.application.credentials.paypal_secret_id
+)
 PayPal::SDK.logger = Rails.logger

6420902cb2c888f5bda0350eaf5a0c22c89d8cd9
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-02-05 18:01:52 -0300

    Add translation to datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,21 +20,40 @@
 //= stub active_admin
 //= require_tree .
 document.addEventListener('turbolinks:load',() => {
+  let datepicker_esp = {
+    closeText: 'Cerrar',
+    prevText: 'Anterior',
+    nextText: 'Siguiente',
+    currentText: 'Hoy',
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
+    weekHeader: 'Sm',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''
+  }
+
   $('.based-datepicker').datepicker( {dateFormat: 'dd/mm/yy'} );
   [].slice.call(document.getElementsByClassName('datepicker')).forEach((picker) => {
     let based = [].slice
                   .call(document.getElementsByClassName('based-datepicker'))
                   .find((b) => b.dataset.based == picker.id);
     if (based == undefined) {
-      $('.datepicker').datepicker({ minDate: new Date(), dateFormat: 'dd/mm/yy' });
+      $('.datepicker').datepicker($.extend({minDate: new Date()}, datepicker_esp));
     }else{
-      $('.datepicker').datepicker({
-        minDate: new Date(),
-        dateFormat: 'dd/mm/yy',
-        onSelect: (dateText, inst) => {
-          $(based).datepicker("option", "minDate", picker.value);
-        }
-      });
+      $('.datepicker').datepicker($.extend(
+        {
+          minDate: new Date(),
+          dateFormat: 'dd/mm/yy',
+          onSelect: (dateText, inst) => {
+            $(based).datepicker("option", "minDate", picker.value);
+          }
+        }, datepicker_esp));
       $(based).datepicker("option", "minDate", picker.value);
     }
   });

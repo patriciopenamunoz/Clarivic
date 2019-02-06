ac68fe84fe57e45580d7dcd32196ddce08b3bb72
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-02-06 10:00:54 -0300

    Add validations: user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @hostel_registration = current_user.hostel_registrations.find_by(hostel_id: params[:hostel_id])
-    Comment.create(hostel_registration: @hostel_registration, message: params[:message])
-    redirect_to hostel_path(@hostel_registration.hostel), notice: 'Comentario registrado correctamente.'
+    if Comment.create(hostel_registration: @hostel_registration, message: params[:message])
+      redirect_to hostel_path(@hostel_registration.hostel), notice: 'Comentario registrado correctamente.'
+    else
+      redirect_to hostel_path(@hostel_registration.hostel), alert: 'Comentario no se registró.'
+    end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  validates :message, presence: true
+
   belongs_to :hostel_registration
   has_one :hostel, through: :hostel_registration
   has_one :user, through: :hostel_registration

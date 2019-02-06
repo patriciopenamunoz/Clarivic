da9846db66f5ceb3f5b46a28ebc87b6ce5da0204
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-02-05 23:11:17 -0300

    Change mode to sandbox.

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,5 +1,6 @@
 PayPal::SDK.configure(
   client_id: Rails.application.credentials.paypal_client_id,
-  client_secret: Rails.application.credentials.paypal_secret_id
+  client_secret: Rails.application.credentials.paypal_secret_id,
+  mode: 'sandbox'
 )
 PayPal::SDK.logger = Rails.logger
